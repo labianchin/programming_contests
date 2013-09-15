252babd97f16b191bc8d98635675183007a46ecf
Luis Armando Bianchin <luis.armandob@gmail.com>
2013-09-15 13:26:08 -0300

    Adding cpp template

---
+++ template.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <cassert>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<string> vs;
+typedef pair<int,int> ii;
+typedef vector<ii> vii; 
+
+#define FOR(k,a,b) for(typeof(a) k=(a); k < (b); k++)
+#define REP(k,a) for(unsigned int k=0; k < (a); ++k)
+#define all(c) (c).begin(), (c).end()  
+#define pb push_back 
+#define mp make_pair 
+#define pbp(x, y) pb(mp(x,y))
+#define sz(a) int((a).size()) 
+#define INF 2000000000
+#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)  //iterate a container
+#define present(c,x) ((c).find(x) != (c).end()) 
+#define cpresent(c,x) (find(all(c),x) != (c).end()) 
+#define WATCH(x) {cout<<__FILE__<<":"<<__LINE__<<":WATCHING:"<<#x<<": ";tr(x, it){cout<<*it<<", ";}cout<<endl;}
+
+#define READ2(x,y) while ( (cin >> x) && (cin >> y) && (x|y) )
+#define READCase int cases; cin >> cases; while (cases--)
+//vector< vii > g;
+#define edge(i, j, w) (g[i].pb(mp(j, w)); g[j].pb(mp(i, w));)
+
+int main(){
+//	freopen("in","r",stdin);
+}
